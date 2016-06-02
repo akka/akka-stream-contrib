2ee28063c0a342f7eaf8a9132aa1827e6756a603
Anand Shah <anand.shah110@gmail.com>
2016-06-02 18:28:47 +0530

    Added duplicate element sorting support
    1. Documentation cleanup
    2. Supporting duplicate elements not using TreeSet
    3. Added sort periodically test case & implementation
    4. Validation for duration = 0 microsecond

--- src/main/scala/akka/stream/contrib/SortWithin.scala
+++ src/main/scala/akka/stream/contrib/SortWithin.scala
@@ -7,13 +7,21 @@ import akka.stream.Attributes
 import akka.stream.impl.fusing.GraphStages.SimpleLinearGraphStage
 import akka.stream.stage.{GraphStageLogic, InHandler, OutHandler, TimerGraphStageLogic}
 
-import scala.collection.immutable
-import scala.concurrent.duration.FiniteDuration
+import scala.collection.immutable.VectorBuilder
+import scala.concurrent.duration.{Duration, FiniteDuration}
 
 object SortWithin {
 
   /**
-    * Factory for [[SortWithin]] instances.
+    * Sorts elements received within given duration and emits them at the end of each duration.
+    *
+    * '''Emits when''' at regular interval of finite duration timeout
+    *
+    * '''Backpressures when''' downstream backpressures
+    *
+    * '''Completes when''' upstream completes
+    *
+    * '''Cancels when''' downstream cancels
     *
     * @param finiteDuration duration within which sorting needs to be done. Ideally should be kept as small as possible
     * @tparam A input type
@@ -22,7 +30,15 @@ object SortWithin {
   def apply[A <: Comparable[A]](finiteDuration: FiniteDuration): SortWithin[A] = new SortWithin[A](finiteDuration)
 
   /**
-    * Java API: Factory for [[SortWithin]] instances.
+    * Java API: Sorts elements received within given duration and emits them at the end of each duration.
+    *
+    * '''Emits when''' at regular interval of finite duration timeout
+    *
+    * '''Backpressures when''' downstream backpressures
+    *
+    * '''Completes when''' upstream completes
+    *
+    * '''Cancels when''' downstream cancels
     *
     * @param finiteDuration duration within which sorting needs to be done. Ideally should be kept as small as possible
     * @tparam A input type
@@ -32,41 +48,44 @@ object SortWithin {
 }
 
 final class SortWithin[T <: Comparable[T]](timeout: FiniteDuration) extends SimpleLinearGraphStage[T] {
+  require(timeout > Duration.Zero)
 
-  var sortedElements = immutable.TreeSet[T]()
+  private val buf: VectorBuilder[T] = new VectorBuilder
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new TimerGraphStageLogic(shape) {
     setHandler(in, new InHandler {
       override def onPush(): Unit = {
-        val element: T = grab(in)
-        sortedElements += element
+        buf += grab(in)
         pull(in)
       }
 
-      @scala.throws[Exception](classOf[Exception])
       override def onUpstreamFinish(): Unit = {
-        emitMultiple(out, sortedElements.iterator)
+        emitMultiple(out, buf.result().sorted)
+        buf.clear()
         super.onUpstreamFinish()
       }
 
-      @scala.throws[Exception](classOf[Exception])
       override def onUpstreamFailure(ex: Throwable): Unit = {
-        emitMultiple(out, sortedElements.iterator)
+        emitMultiple(out, buf.result().sorted)
+        buf.clear()
         super.onUpstreamFailure(ex)
       }
     })
 
     setHandler(out, new OutHandler {
-      override def onPull(): Unit = pull(in)
+      override def onPull(): Unit = {}
     })
 
 
     final override protected def onTimer(key: Any): Unit = {
-      emitMultiple(out, sortedElements.iterator)
-      completeStage()
+      emitMultiple(out, buf.result().sorted)
+      buf.clear()
     }
 
-    override def preStart(): Unit = scheduleOnce("SortWithinTimer", timeout)
+    override def preStart(): Unit = {
+      schedulePeriodically("SortWithinTimer", timeout)
+      pull(in)
+    }
   }
 
   override def toString = "SortWithin"
--- src/test/scala/akka/stream/contrib/SortWithinSpec.scala
+++ src/test/scala/akka/stream/contrib/SortWithinSpec.scala
@@ -5,7 +5,8 @@ package akka.stream.contrib
 
 import java.util.concurrent.TimeUnit
 
-import akka.stream.scaladsl.Keep
+import akka.stream.DelayOverflowStrategy
+import akka.stream.scaladsl.{Keep, Source}
 import akka.stream.testkit.scaladsl.{TestSink, TestSource}
 
 import scala.concurrent.duration.FiniteDuration
@@ -34,9 +35,7 @@ trait SortWithinSpec extends BaseStreamSpec {
       sink.expectNext(FiniteDuration.apply(6, TimeUnit.SECONDS), 2)
       sink.expectNext(FiniteDuration.apply(6, TimeUnit.SECONDS), 3)
     }
-  }
 
-  "A stream via SortWithin" should {
     "sort the elements till upstream completion" in {
       val (source, sink) = TestSource.probe[Integer]
         .via(SortWithin[Integer](FiniteDuration.apply(20, TimeUnit.SECONDS)))
@@ -45,26 +44,24 @@ trait SortWithinSpec extends BaseStreamSpec {
       sink.request(99)
       source.sendNext(1)
       source.sendNext(3)
-      source.sendNext(2)
+      source.sendNext(1)
       source.sendComplete()
-      sink.expectNext(1, 2, 3)
+      sink.expectNext(1, 1, 3)
       sink.expectComplete()
     }
-  }
 
-  "A stream via SortWithin" should {
-    "sort the elements till exception" in {
-      val (source, sink) = TestSource.probe[Integer]
-        .via(SortWithin[Integer](FiniteDuration.apply(2, TimeUnit.SECONDS)))
-        .toMat(TestSink.probe)(Keep.both)
-        .run()
-      sink.request(99)
-      source.sendNext(3)
-      source.sendNext(1)
-      val cause: RuntimeException = new RuntimeException
-      source.sendError(cause)
-      sink.expectNext(FiniteDuration.apply(3, TimeUnit.SECONDS), 1)
-      sink.expectError(cause)
+    "sort elements for every finite duration" in {
+      val duration: FiniteDuration = FiniteDuration.apply(1, TimeUnit.SECONDS)
+      Source((1 to 10).reverse).map(Integer.valueOf).delay(duration, DelayOverflowStrategy.backpressure)
+        .via(SortWithin[Integer](FiniteDuration.apply(5, TimeUnit.SECONDS)))
+        .runWith(TestSink.probe[Integer])
+        .request(10)
+        .expectNoMsg(FiniteDuration.apply(4700, TimeUnit.MILLISECONDS))
+        .expectNext(7, 8, 9, 10)
+        .expectNoMsg(FiniteDuration.apply(4600, TimeUnit.MILLISECONDS))
+        .expectNext(2, 3, 4, 5, 6)
+        .expectNext(1)
+        .expectComplete()
     }
   }
 }
