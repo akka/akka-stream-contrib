77a8f632d13abb441379864319848c0cbe99984f
Tal Pressman <kirika@gmail.com>
2016-08-26 14:26:53 +0300

    This works, but has no support for materialized values

--- contrib/src/main/scala/akka/stream/contrib/FlowTagger.scala
+++ contrib/src/main/scala/akka/stream/contrib/FlowTagger.scala
@@ -1,8 +1,9 @@
 package akka.stream.contrib
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream._
-import akka.stream.scaladsl.{Flow, FlowOpsMat, GraphDSL, Keep, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Flow, FlowOps, GraphDSL, RunnableGraph, Sink, Source}
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
 
 import scala.annotation.unchecked.uncheckedVariance
@@ -96,7 +97,7 @@ object FocusedFlow {
     val foo = Source(List((1l, "one"), (2l, "two"), (3l, "three"), (4l, "four")))
       .focus
         .map(_.toUpperCase)
-        .filterNot(_.startsWith("O"))
+        .filterNot(_.startsWith("T"))
       .unfocus
       .runWith(Sink.foreach(println))
 
@@ -104,88 +105,64 @@ object FocusedFlow {
   }
 }
 
-class FocusedFlow[Tag, SrcIn, SrcMat, -In, +Out, +Mat](
+class FocusedFlow[Tag, SrcIn, SrcMat, -In, +Out](
    src: Flow[SrcIn, (Tag, In), SrcMat],
-   innerFlow: Flow[In, Out, Mat]
-   ) extends FlowOpsMat[Out, Mat] {
+   innerFlow: Flow[In, Out, NotUsed]
+   ) extends FlowOps[Out, NotUsed] {
 
-  override type Repr[+O] = FocusedFlow[Tag, SrcIn, SrcMat, In@uncheckedVariance, O, Mat@uncheckedVariance]
-  override type ReprMat[+O, +M] = FocusedFlow[Tag, SrcIn, SrcMat, In@uncheckedVariance, O, M]
+  override type Repr[+O] = FocusedFlow[Tag, SrcIn, SrcMat, In@uncheckedVariance, O]
   override type Closed = Sink[SrcIn, SrcMat]
-  override type ClosedMat[+M] = Sink[SrcIn@uncheckedVariance, M]
 
   def unfocus: Flow[SrcIn, (Tag, Out), SrcMat] =
     src.via(FlowTagger.tagged(innerFlow))
 
-  def unfocusMat[Mat2](combine: (SrcMat, Mat) => Mat2): Flow[SrcIn, (Tag, Out), Mat2] =
-    src.viaMat(FlowTagger.tagged(innerFlow))(combine)
+  override def via[T, Mat2](flow: Graph[FlowShape[Out, T], Mat2]): FocusedFlow[Tag, SrcIn, SrcMat, In, T] =
+    new FocusedFlow(src, innerFlow.via(flow))
 
-  override def via[T, Mat2](flow: Graph[FlowShape[Out, T], Mat2]): FocusedFlow[Tag, SrcIn, SrcMat, In, T, Mat] =
-    viaMat(flow)(Keep.left)
-  override def viaMat[T, Mat2, Mat3](flow: Graph[FlowShape[Out, T], Mat2])(combine: (Mat, Mat2) => Mat3): FocusedFlow[Tag, SrcIn, SrcMat, In, T, Mat3] =
-    new FocusedFlow(src, innerFlow.viaMat(flow)(combine))
-
-  // connecting the focused flow to a sink will discard the tag and the original materialized value!
+  // connecting the focused flow to a sink will discard the tag
   override def to[Mat2](sink: Graph[SinkShape[Out], Mat2]): Sink[SrcIn, SrcMat] =
     src.map(_._2).via(innerFlow).to(sink)
-  override def toMat[Mat2, Mat3](sink: Graph[SinkShape[Out], Mat2])(combine: (Mat, Mat2) => Mat3): Sink[SrcIn, Mat3] =
-    src.map(_._2).toMat(innerFlow.toMat(sink)(combine))(Keep.right)
 
-  override def mapMaterializedValue[Mat2](f: (Mat) => Mat2): FocusedFlow[Tag, SrcIn, SrcMat, In, Out, Mat2] =
-    new FocusedFlow(src, innerFlow.mapMaterializedValue(f))
-
-  override def withAttributes(attr: Attributes): FocusedFlow[Tag, SrcIn, SrcMat, In, Out, Mat] =
+  override def withAttributes(attr: Attributes): FocusedFlow[Tag, SrcIn, SrcMat, In, Out] =
     new FocusedFlow(src, innerFlow.withAttributes(attr))
 
-  override def addAttributes(attr: Attributes): FocusedFlow[Tag, SrcIn, SrcMat, In, Out, Mat] =
+  override def addAttributes(attr: Attributes): FocusedFlow[Tag, SrcIn, SrcMat, In, Out] =
     new FocusedFlow(src, innerFlow.addAttributes(attr))
 
-  override def named(name: String): FocusedFlow[Tag, SrcIn, SrcMat, In, Out, Mat] =
+  override def named(name: String): FocusedFlow[Tag, SrcIn, SrcMat, In, Out] =
     new FocusedFlow(src, innerFlow.named(name))
 
-  override def async: FocusedFlow[Tag, SrcIn, SrcMat, In, Out, Mat] =
+  override def async: FocusedFlow[Tag, SrcIn, SrcMat, In, Out] =
     new FocusedFlow(src, innerFlow.async)
 }
 
-class FocusedSource[Tag, SrcMat, -In, +Out, +Mat](
+class FocusedSource[Tag, SrcMat, -In, +Out](
   src: Source[(Tag, In), SrcMat],
-  innerFlow: Flow[In, Out, Mat]
-  ) extends FlowOpsMat[Out, Mat] {
+  innerFlow: Flow[In, Out, NotUsed]
+  ) extends FlowOps[Out, NotUsed] {
 
-  override type Repr[+O] = FocusedSource[Tag, SrcMat, In@uncheckedVariance, O, Mat@uncheckedVariance]
-  override type ReprMat[+O, +M] = FocusedSource[Tag, SrcMat, In@uncheckedVariance, O, M]
+  override type Repr[+O] = FocusedSource[Tag, SrcMat, In@uncheckedVariance, O]
   override type Closed = RunnableGraph[SrcMat]
-  override type ClosedMat[+M] = RunnableGraph[M]
 
   def unfocus: Source[(Tag, Out), SrcMat] =
     src.via(FlowTagger.tagged(innerFlow))
 
-  def unfocusMat[Mat2](combine: (SrcMat, Mat) => Mat2): Source[(Tag, Out), Mat2] =
-    src.viaMat(FlowTagger.tagged(innerFlow))(combine)
+  override def via[T, Mat2](flow: Graph[FlowShape[Out, T], Mat2]): FocusedSource[Tag, SrcMat, In, T] =
+    new FocusedSource(src, innerFlow.via(flow))
 
-  override def via[T, Mat2](flow: Graph[FlowShape[Out, T], Mat2]): FocusedSource[Tag, SrcMat, In, T, Mat] =
-    viaMat(flow)(Keep.left)
-  override def viaMat[T, Mat2, Mat3](flow: Graph[FlowShape[Out, T], Mat2])(combine: (Mat, Mat2) => Mat3): FocusedSource[Tag, SrcMat, In, T, Mat3] =
-    new FocusedSource(src, innerFlow.viaMat(flow)(combine))
-
-  // connecting the focused flow to a sink will discard the tag and the original materialized value!
+  // connecting the focused flow to a sink will discard the tag
   override def to[Mat2](sink: Graph[SinkShape[Out], Mat2]): RunnableGraph[SrcMat] =
     src.map(_._2).via(innerFlow).to(sink)
-  override def toMat[Mat2, Mat3](sink: Graph[SinkShape[Out], Mat2])(combine: (Mat, Mat2) => Mat3): RunnableGraph[Mat3] =
-    src.map(_._2).toMat(innerFlow.toMat(sink)(combine))(Keep.right)
 
-  override def mapMaterializedValue[Mat2](f: (Mat) => Mat2): FocusedSource[Tag, SrcMat, In, Out, Mat2] =
-    new FocusedSource(src, innerFlow.mapMaterializedValue(f))
-
-  override def withAttributes(attr: Attributes): FocusedSource[Tag, SrcMat, In, Out, Mat] =
+  override def withAttributes(attr: Attributes): FocusedSource[Tag, SrcMat, In, Out] =
     new FocusedSource(src, innerFlow.withAttributes(attr))
 
-  override def addAttributes(attr: Attributes): FocusedSource[Tag, SrcMat, In, Out, Mat] =
+  override def addAttributes(attr: Attributes): FocusedSource[Tag, SrcMat, In, Out] =
     new FocusedSource(src, innerFlow.addAttributes(attr))
 
-  override def named(name: String): FocusedSource[Tag, SrcMat, In, Out, Mat] =
+  override def named(name: String): FocusedSource[Tag, SrcMat, In, Out] =
     new FocusedSource(src, innerFlow.named(name))
 
-  override def async: FocusedSource[Tag, SrcMat, In, Out, Mat] =
+  override def async: FocusedSource[Tag, SrcMat, In, Out] =
     new FocusedSource(src, innerFlow.async)
 }
