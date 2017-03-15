b2512610895ef1c1978da00eb466bd03e59e9b28
Marcin Rzeźnicki <mrzeznicki@iterato.rs>
2017-03-15 04:16:53 +0100

    Add Pulse stage

---
+++ contrib/src/main/scala/akka/stream/contrib/Pulse.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.stream.contrib
+
+import akka.stream.Attributes
+import akka.stream.impl.fusing.GraphStages.SimpleLinearGraphStage
+import akka.stream.stage._
+
+import scala.concurrent.duration.FiniteDuration
+
+/**
+ * Signals demand only once every [[interval]] (''pulse'') and then back-pressures. Requested element is emitted downstream if there is demand.
+ *
+ * @param interval ''pulse'' period
+ * @param initiallyOpen if `true` - emits the first available element before ''pulsing''
+ * @tparam T type of element
+ */
+final class Pulse[T](val interval: FiniteDuration, val initiallyOpen: Boolean = false) extends SimpleLinearGraphStage[T] {
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new TimerGraphStageLogic(shape) with InHandler with OutHandler {
+
+      setHandlers(in, out, this)
+
+      override def preStart(): Unit = if (!initiallyOpen) startPulsing()
+      override def onPush(): Unit = if (isAvailable(out)) push(out, grab(in))
+      override def onPull(): Unit = if (!pulsing) {
+        pull(in)
+        startPulsing()
+      }
+
+      override protected def onTimer(timerKey: Any): Unit = {
+        if (!isClosed(in) && !hasBeenPulled(in)) pull(in)
+      }
+
+      private def startPulsing() = {
+        pulsing = true
+        schedulePeriodically("PulseTimer", interval)
+      }
+      private var pulsing = false
+    }
+
+  override def toString = "Pulse"
+}
