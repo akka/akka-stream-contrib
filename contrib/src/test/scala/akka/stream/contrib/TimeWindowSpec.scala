a54f91639d69b6a072b1e35cf53d13803c5dc4cd
Martynas Mickevičius <martynas@2m.lt>
2017-05-28 20:01:12 +0300

    #104 Use mock scheduler when testing Pulse stage

--- contrib/src/test/scala/akka/stream/contrib/TimeWindowSpec.scala
+++ contrib/src/test/scala/akka/stream/contrib/TimeWindowSpec.scala
@@ -3,65 +3,74 @@
  */
 package akka.stream.contrib
 
-import akka.stream.scaladsl.Keep
-import akka.stream.testkit.scaladsl.{ TestSink, TestSource }
-import akka.testkit.TestDuration
-import org.scalatest.concurrent.ScalaFutures
+import java.util.concurrent.ThreadFactory
+
+import akka.event.LoggingAdapter
+import akka.stream.scaladsl.Source
+import akka.stream.testkit.scaladsl.TestSink
+import com.miguno.akka.testing.{ MockScheduler, VirtualTime }
+import com.typesafe.config.{ Config, ConfigFactory }
 
 import scala.concurrent.duration._
 
-/*
-conflateWithSeed seems not to work as expected because of buffering of each stage when auto-fusing=off
 class TimeWindowSpecAutoFusingOff extends { val autoFusing = false } with TimeWindowSpec
- */
 class TimeWindowSpecAutoFusingOn extends { val autoFusing = true } with TimeWindowSpec
 
-trait TimeWindowSpec extends BaseStreamSpec with ScalaFutures {
-  private val timeWindow = 100.milliseconds
+class AkkaMockScheduler extends {
+  val time = new VirtualTime
+} with MockScheduler(time) {
+  def this(config: Config, adapter: LoggingAdapter, tf: ThreadFactory) = this()
+}
+
+trait TimeWindowSpec extends BaseStreamSpec {
+
+  override def config = ConfigFactory.parseString(
+    s"""
+      |akka.scheduler.implementation = ${classOf[AkkaMockScheduler].getName}
+    """.stripMargin
+  )
+
+  private val timeWindow = 100.millis
+  private val epsilonTime = 10.millis
+
+  private val scheduler = system.scheduler.asInstanceOf[AkkaMockScheduler]
 
   "TimeWindow flow" should {
     "aggregate data for predefined amount of time" in {
-      val summingWindow = TimeWindow(timeWindow.dilated, eager = false)(identity[Int])(_ + _)
+      val summingWindow = TimeWindow(timeWindow, eager = false)(identity[Int])(_ + _)
 
-      val (pub, sub) = TestSource.probe[Int]
+      val sub = Source.repeat(1)
         .via(summingWindow)
-        .toMat(TestSink.probe)(Keep.both)
-        .run
+        .runWith(TestSink.probe)
 
       sub.request(2)
 
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      sub.expectNext(timeWindow * 2, 5)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      sub.expectNext(timeWindow * 2, 5)
+      sub.expectNoMsg(timeWindow + epsilonTime)
+      scheduler.time.advance(timeWindow + epsilonTime)
+      scheduler.tick()
+      sub.expectNext()
+
+      sub.expectNoMsg(timeWindow + epsilonTime)
+      scheduler.time.advance(timeWindow + epsilonTime)
+      scheduler.tick()
+      sub.expectNext()
     }
 
     "emit the first seed if eager" in {
-      val summingWindow = TimeWindow(timeWindow.dilated, eager = true)(identity[Int])(_ + _)
+      val summingWindow = TimeWindow(timeWindow, eager = true)(identity[Int])(_ + _)
 
-      val (pub, sub) = TestSource.probe[Int]
+      val sub = Source.repeat(1)
         .via(summingWindow)
-        .toMat(TestSink.probe)(Keep.both)
-        .run
+        .runWith(TestSink.probe)
 
       sub.request(2)
 
-      pub.sendNext(1)
-      sub.expectNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      pub.sendNext(1)
-      sub.expectNext(5)
+      sub.expectNext()
+
+      sub.expectNoMsg(timeWindow + epsilonTime)
+      scheduler.time.advance(timeWindow + epsilonTime)
+      scheduler.tick()
+      sub.expectNext()
     }
   }
 }
