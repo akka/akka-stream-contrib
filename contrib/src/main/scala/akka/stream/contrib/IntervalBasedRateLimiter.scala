733b0cc0fe165b75280755174d91dd5de6801f25
Arnout Engelen <arnouten@bzzt.net>
2018-02-14 12:00:38 +0100

    Fix formatting

--- contrib/src/main/scala/akka/stream/contrib/IntervalBasedRateLimiter.scala
+++ contrib/src/main/scala/akka/stream/contrib/IntervalBasedRateLimiter.scala
@@ -13,17 +13,17 @@ import scala.concurrent.duration._
 object IntervalBasedRateLimiter {
 
   /**
-    * Specialized type of rate limiter which emits batches of elements (with size limited by the @maxBatchSize parameter)
-    * with a minimum time interval of @minInterval.
-    *
-    * Because the next emit is scheduled after we downstream the current batch, the effective throughput,
-    * depending on the minimal interval length, may never reach the maximum allowed one.
-    * You can minimize these delays by sending bigger batches less often.
-    *
-    * @param minInterval  minimal pause to be kept before downstream the next batch. Should be >= 10 milliseconds.
-    * @param maxBatchSize maximum number of elements to send in the single batch
-    * @tparam T type of element
-    */
+   * Specialized type of rate limiter which emits batches of elements (with size limited by the @maxBatchSize parameter)
+   * with a minimum time interval of @minInterval.
+   *
+   * Because the next emit is scheduled after we downstream the current batch, the effective throughput,
+   * depending on the minimal interval length, may never reach the maximum allowed one.
+   * You can minimize these delays by sending bigger batches less often.
+   *
+   * @param minInterval  minimal pause to be kept before downstream the next batch. Should be >= 10 milliseconds.
+   * @param maxBatchSize maximum number of elements to send in the single batch
+   * @tparam T type of element
+   */
   def create[T](minInterval: FiniteDuration, maxBatchSize: Int): Graph[FlowShape[T, immutable.Seq[T]], NotUsed] =
     Flow[T].groupedWithin(maxBatchSize, minInterval).via(DelayFlow[immutable.Seq[T]](minInterval))
 
