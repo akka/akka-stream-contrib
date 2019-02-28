acc16027551ecdeee74f64d5e6c9e1c190da760c
Tomas Mikula <tomas.mikula@gmail.com>
2019-02-28 19:09:23 +0100

    Make the partitionWith test less trivial using Zip.

--- contrib/src/test/scala/akka/stream/contrib/PartitionWithSpec.scala
+++ contrib/src/test/scala/akka/stream/contrib/PartitionWithSpec.scala
@@ -4,8 +4,9 @@
 
 package akka.stream.contrib
 
+import akka.NotUsed
 import akka.stream.scaladsl._
-import akka.stream.{ ClosedShape, FanOutShape2, FlowShape, Graph }
+import akka.stream.{ ClosedShape, FanInShape2, FanOutShape2, FlowShape, Graph }
 import akka.stream.testkit.scaladsl.{ TestSink, TestSource }
 
 class PartitionWithSpecAutoFusingOn extends { val autoFusing = true } with PartitionWithSpec
@@ -13,8 +14,24 @@ class PartitionWithSpecAutoFusingOff extends { val autoFusing = false } with Par
 
 trait PartitionWithSpec extends BaseStreamSpec {
 
+  private def fanOutAndIn[I, X, Y, O, M](
+    fanOutGraph: Graph[FanOutShape2[I, X, Y], M],
+    fanInGraph:  Graph[FanInShape2[X, Y, O], NotUsed]): Flow[I, O, M] = {
+    Flow.fromGraph(GraphDSL.create(fanOutGraph, fanInGraph)(Keep.left) { implicit builder => (fanOut, fanIn) =>
+      import GraphDSL.Implicits._
+
+      fanOut.out0 ~> fanIn.in0
+      fanOut.out1 ~> fanIn.in1
+
+      FlowShape(fanOut.in, fanIn.out)
+    })
+  }
+
+  private def zipFanOut[I, O1, O2, M](fanOutGraph: Graph[FanOutShape2[I, O1, O2], M]): Flow[I, (O1, O2), M] =
+    fanOutAndIn(fanOutGraph, Zip[O1, O2])
+
   private def mergeFanOut[I, O, M](fanOutGraph: Graph[FanOutShape2[I, O, O], M]): Flow[I, O, M] = {
-    Flow.fromGraph(GraphDSL.create(fanOutGraph) { implicit builder => fanOut => {
+    Flow.fromGraph(GraphDSL.create(fanOutGraph) { implicit builder => fanOut =>
       import GraphDSL.Implicits._
 
       val mrg = builder.add(Merge[O](2))
@@ -23,7 +40,6 @@ trait PartitionWithSpec extends BaseStreamSpec {
       fanOut.out1 ~> mrg.in(1)
 
       FlowShape(fanOut.in, mrg.out)
-    }
     })
   }
 
@@ -101,7 +117,7 @@ trait PartitionWithSpec extends BaseStreamSpec {
     "be callable on Flow and partition its output" in {
       import PartitionWith.Implicits._
 
-      val flow = mergeFanOut(
+      val flow = zipFanOut(
         Flow[Int].partitionWith(i => if (i % 2 == 0) Left(-i) else Right(i)))
 
       val (source, sink) = TestSource.probe[Int]
@@ -109,9 +125,9 @@ trait PartitionWithSpec extends BaseStreamSpec {
         .toMat(TestSink.probe)(Keep.both)
         .run()
 
-      sink.request(10)
+      sink.request(5)
       (1 to 10).foreach(source.sendNext)
-      sink.expectNextN(List(1, -2, 3, -4, 5, -6, 7, -8, 9, -10))
+      sink.expectNextN(List((-2, 1), (-4, 3), (-6, 5), (-8, 7), (-10, 9)))
     }
   }
 }
